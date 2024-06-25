9a9d78e8d8cfe280a0789a52fa1a1e527215ccfc
helloredreyes <hello.silentlymine@gmail.com>
2024-06-25 14:50:04 +0700

    sync

---
+++ scripts/menu.js
@@ -0,0 +1,17 @@
+// pdf menu
+function openMenu(evt, menuName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it to display the default tab content
+document.getElementById("defaultOpen").click();
